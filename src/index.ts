0cd3ffe26a447145e9e863f78a36e51ea948aebd
maeta187 <nexus18789@gmail.com>
2024-03-10 23:38:28 +0900

    ✨ : Basic認証の実装

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import { Hono } from 'hono';
 import { todos } from './todos/api';
+import { basicAuth } from 'hono/basic-auth';
 
 const app = new Hono();
+
+app.use(
+	'/api/*',
+	basicAuth({
+		username: 'admin',
+		password: 'admin',
+	})
+);
+
 app.route('/api/todos', todos);
 
 export default app;
